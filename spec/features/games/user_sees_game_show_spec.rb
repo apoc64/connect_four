9ba3982783e45f534b85ffba1502feb3ada1f0f2
apoc64 <sschwedt@gmail.com>
2018-05-16 23:25:02 -0600

    adds images to game board

--- spec/features/games/user_sees_game_show_spec.rb
+++ spec/features/games/user_sees_game_show_spec.rb
@@ -10,8 +10,14 @@ describe 'user visits the game show page' do
     message7 = "Square 7 is empty"
     message16 = "Square 16 is empty"
 
-    expect(page).to have_content(message1)
-    expect(page).to have_content(message7)
-    expect(page).to have_content(message16)
+    within('.cell1') do
+      expect(page).to have_content(message1)
+    end
+    within('.cell7') do
+      expect(page).to have_content(message7)
+    end
+    within('.cell16') do
+      expect(page).to have_content(message16)
+    end
   end
 end
