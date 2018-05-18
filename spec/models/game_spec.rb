2751c0bd5168b505101ef91e2cb1a68ee59d3752
apoc64 <sschwedt@gmail.com>
2018-05-17 20:59:25 -0600

    dropped cells can stack

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -55,5 +55,39 @@ describe Game, type: :model do
       expect(game.cells[15].value).to eq(0)
       expect(game.cells[15].alt).to eq('empty')
     end
+
+    it 'dropping pieces can stack' do
+      user = User.create(name: 'bob', password: '1234')
+      game = Game.create_game(user)
+      game.drop(0, 1) #(column, player)
+      result = game.drop(0, 2)
+
+      expect(result).to eq(true)
+      expect(game.cells[12].value).to eq(1)
+      expect(game.cells[12].alt).to eq('red')
+      expect(game.cells[13].value).to eq(0)
+      expect(game.cells[13].alt).to eq('empty')
+      expect(game.cells[8].value).to eq(2)
+      expect(game.cells[8].alt).to eq('black')
+      expect(game.cells[14].value).to eq(0)
+      expect(game.cells[14].alt).to eq('empty')
+      expect(game.cells[15].value).to eq(0)
+      expect(game.cells[15].alt).to eq('empty')
+
+      game.drop(2, 2)
+      result = game.drop(2, 1)
+
+      expect(result).to eq(true)
+      expect(game.cells[12].value).to eq(1)
+      expect(game.cells[12].alt).to eq('red')
+      expect(game.cells[13].value).to eq(0)
+      expect(game.cells[13].alt).to eq('empty')
+      expect(game.cells[8].value).to eq(2)
+      expect(game.cells[8].alt).to eq('black')
+      expect(game.cells[14].value).to eq(2)
+      expect(game.cells[14].alt).to eq('black')
+      expect(game.cells[10].value).to eq(1)
+      expect(game.cells[10].alt).to eq('red')
+    end
   end
 end
