0fef718e14751462ae5cdf72b0e9c5ec6b1afccd
apoc64 <sschwedt@gmail.com>
2018-05-17 20:49:49 -0600

    can drop cells to bottom row

--- app/models/game.rb
+++ app/models/game.rb
@@ -10,4 +10,20 @@ class Game <ApplicationRecord
     end
     game
   end
+
+  COLUMNS = [[0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15]]
+
+  def drop(column, player)
+    spaces = COLUMNS[column]
+    return false if cells[spaces[0]].value != 0
+    spaces.each_with_index do |space, index|
+      if index == 3
+        cells[space].update(value: player)
+        return true
+      elsif cells[spaces[index + 1]].value != 0
+        cells[space].update(value: player)
+        return true
+      end
+    end
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -23,4 +23,37 @@ describe Game, type: :model do
       expect(game.cells.count).to eq(16)
     end
   end
+
+  describe 'dropping piece in column' do
+    it 'dropping piece in column goes to bottom' do
+      user = User.create(name: 'bob', password: '1234')
+      game = Game.create_game(user)
+      result = game.drop(0, 1) #(column, player)
+
+      expect(result).to eq(true)
+      expect(game.cells[12].value).to eq(1)
+      expect(game.cells[12].alt).to eq('red')
+      expect(game.cells[13].value).to eq(0)
+      expect(game.cells[13].alt).to eq('empty')
+      expect(game.cells[8].value).to eq(0)
+      expect(game.cells[8].alt).to eq('empty')
+      expect(game.cells[14].value).to eq(0)
+      expect(game.cells[14].alt).to eq('empty')
+      expect(game.cells[15].value).to eq(0)
+      expect(game.cells[15].alt).to eq('empty')
+
+      result = game.drop(2, 2)
+      expect(result).to eq(true)
+      expect(game.cells[12].value).to eq(1)
+      expect(game.cells[12].alt).to eq('red')
+      expect(game.cells[13].value).to eq(0)
+      expect(game.cells[13].alt).to eq('empty')
+      expect(game.cells[8].value).to eq(0)
+      expect(game.cells[8].alt).to eq('empty')
+      expect(game.cells[14].value).to eq(2)
+      expect(game.cells[14].alt).to eq('black')
+      expect(game.cells[15].value).to eq(0)
+      expect(game.cells[15].alt).to eq('empty')
+    end
+  end
 end
