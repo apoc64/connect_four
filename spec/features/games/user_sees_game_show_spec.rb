15db6c55b24f9ae03cbd8860e450dfbc57e9352f
apoc64 <sschwedt@gmail.com>
2018-05-17 20:04:41 -0600

    cells in gameboard can be red or black

--- spec/features/games/user_sees_game_show_spec.rb
+++ spec/features/games/user_sees_game_show_spec.rb
@@ -21,4 +21,28 @@ describe 'user visits the game show page' do
       expect(page).to have_content(message16)
     end
   end
+
+  it 'can show red and black' do
+    user = User.create(name: 'bob', password: '1234')
+    game = user.create_game
+
+    message1 = 'Square 1 is red'
+    game.cells[0].update(value: 1)
+    message7 = 'Square 7 is black'
+    game.cells[6].update(value: 2)
+    message16 = 'Square 16 is red'
+    game.cells[15].update(value: 1)
+
+    visit game_path(game)
+
+    within('.cell1') do
+      expect(page).to have_content(message1)
+    end
+    within('.cell7') do
+      expect(page).to have_content(message7)
+    end
+    within('.cell16') do
+      expect(page).to have_content(message16)
+    end
+  end
 end
