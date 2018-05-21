16df63c6a7faf7ce4cd3529c631e0d5923a5eab7
apoc64 <sschwedt@gmail.com>
2018-05-21 14:47:58 -0600

    passes tests for secure password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ describe User, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:password)}
-    # it {should validate_uniqueness_of(:name)}
-    # it {should have_secure_password}
+    it {should validate_uniqueness_of(:name)}
+    it {should have_secure_password}
   end
 
   describe 'relationships' do
