6b17460c589ecba65d1bbb8000abe209d95e5b31
apoc64 <sschwedt@gmail.com>
2018-05-21 18:21:56 -0600

    logs in new users and redirects failed signups

--- spec/features/users/user_can_create_new_user_spec.rb
+++ spec/features/users/user_can_create_new_user_spec.rb
@@ -1,20 +1,50 @@
 require 'rails_helper'
 
 describe 'user visits new user page' do
-  it 'submitting valid form creates new user' do
+  it 'submitting valid form creates new user and logs them in' do
     visit new_user_path
 
     name = 'bob'
     fill_in 'user[name]', with: name
     fill_in 'user[password]', with: '1234'
+
+    expect(page).to have_link('Log in')
+    expect(page).to_not have_link('Log out')
+
     click_button 'Create'
 
     expect(current_path).to eq(user_path(User.last))
     expect(page).to have_content("Hello, #{name}")
+    expect(page).to have_link('Log out')
+    expect(page).to_not have_link('Log in')
   end
 
-  xit 'fails on blank field' do
+  it 'fails on blank field' do
+    visit new_user_path
+
+    name = 'bob'
+    fill_in 'user[name]', with: name
+    click_button 'Create'
+
+    message = 'User creation failed'
+
+    expect(current_path).to eq(new_user_path)
+    expect(page).to have_content(message)
   end
-  xit 'fails on non-unique name' do
+
+  it 'fails on non-unique name' do
+    visit new_user_path
+
+    name = 'bob'
+    password = '1234'
+    user1 = User.create(name: name, password: password)
+    fill_in 'user[name]', with: name
+    fill_in 'user[password]', with: '12345'
+    click_button 'Create'
+
+    message = 'User creation failed'
+
+    expect(current_path).to eq(new_user_path)
+    expect(page).to have_content(message)
   end
 end
