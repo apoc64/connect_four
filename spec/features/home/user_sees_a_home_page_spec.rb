efffc53a468da89fcb8e4d76844ebf329216059e
apoc64 <sschwedt@gmail.com>
2018-05-22 15:48:56 -0600

    admins have additional nav items

--- spec/features/home/user_sees_a_home_page_spec.rb
+++ spec/features/home/user_sees_a_home_page_spec.rb
@@ -7,9 +7,50 @@ describe 'User' do
     expect(page).to have_content('Connect 4!')
     expect(page).to have_link(href: login_path)
     expect(page).to have_link(href: new_user_path)
+
+    expect(page).to_not have_link(href: logout_path)
+    expect(page).to_not have_link("Your Profile")
+
+    expect(page).to_not have_link(href: admin_users_path)
+    expect(page).to_not have_link(href: admin_trophies_path)
     # empty game board
   end
 
-  xit 'it shows different links when logged in' do
+  it 'it shows different links when logged in' do
+    user = User.create(name: 'bob', password: '1234')
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit root_path
+
+    expect(page).to have_content('Connect 4!')
+    expect(page).to_not have_link(href: login_path)
+    expect(page).to_not have_link(href: new_user_path)
+
+    expect(page).to have_link(href: logout_path)
+    expect(page).to have_link(href: user_path(user))
+
+    expect(page).to_not have_link(href: admin_users_path)
+    expect(page).to_not have_link(href: admin_trophies_path)
   end
+
+  it 'it shows admin links for an admin' do
+    admin = User.create(name: 'bob', password: '1234', role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit root_path
+
+    expect(page).to have_content('Connect 4!')
+    expect(page).to_not have_link(href: login_path)
+    expect(page).to_not have_link(href: new_user_path)
+
+    expect(page).to have_link(href: logout_path)
+    expect(page).to have_link(href: user_path(admin))
+
+    expect(page).to have_link(href: admin_users_path)
+    expect(page).to have_link(href: admin_trophies_path)
+  end
+
+
 end
