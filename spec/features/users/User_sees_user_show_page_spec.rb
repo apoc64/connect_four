81e2a58531bd9793af7fb44d808dc3c59c428812
apoc64 <sschwedt@gmail.com>
2018-05-22 13:19:03 -0600

    users can only view their own show page when logged in

--- spec/features/users/User_sees_user_show_page_spec.rb
+++ spec/features/users/User_sees_user_show_page_spec.rb
@@ -1,9 +1,41 @@
 require 'rails_helper'
 
 describe 'user visits user show page' do
-  it 'can create a new game' do
+  it 'throws a 404 if not logged in' do
     user = User.create(name: 'bob', password: '1234')
 
+    visit user_path(user)
+    message = "These are not the droids you're looking for"
+
+    expect(page).to have_content(message)
+  end
+
+  it 'throws a 404 if not the correct user' do
+    user1 = User.create(name: 'bob', password: '1234')
+    user2 = User.create(name: 'blob', password: '1234')
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+
+    visit user_path(user2)
+    message = "These are not the droids you're looking for"
+
+    expect(page).to have_content(message)
+  end
+
+  it 'shows a message if the right user logged in' do
+    name = 'bob'
+    user = User.create(name: name, password: '1234')
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_path(user)
+
+    expect(page).to have_content("Hello, #{name}")
+  end
+
+  it 'can create a new game' do
+    user = User.create(name: 'bob', password: '1234')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit user_path(user)
 
     click_on 'Start New Game'
@@ -34,6 +66,7 @@ describe 'user visits user show page' do
     message2 = "Losses: 2"
     message3 = "Draws: 1"
     message4 = "In Progress: 4"
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit user_path(user)
 
     expect(page).to have_content(message1)
