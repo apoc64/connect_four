52cccb98d2e82f191eff395ded72626dbbb562e7
apoc64 <sschwedt@gmail.com>
2018-05-24 20:10:24 -0600

    shows trophies, allows delete

--- app/models/trophy.rb
+++ app/models/trophy.rb
@@ -2,5 +2,7 @@ class Trophy < ApplicationRecord
   validates_presence_of :name, :description, :image
 
   has_many :user_trophies
-  has_many :users, through: :user_trophies
+  has_many :users, through: :user_trophies, dependent: :destroy
+
+
 end
