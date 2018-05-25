718080ed14ec017aa3ec7556350b85201fc5d9f9
apoc64 <sschwedt@gmail.com>
2018-05-24 20:47:18 -0600

    high scrores

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,32 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.select('count(user.games.where(status = 1)) AS count').group(:user_id).order(:count)
+    # @users = User.select("users.*, count(games.id) as wins").join(:games).group(:user).order("wins desc")
+    # binding.pry
+    #User.select('count(user.games.where(status = 1)) AS count').group(:user_id).order(:count)
+    users = User.all
+    mapped_users = users.map do |u|
+      [u, u.games.where(status: 1).count]
+    end
+    @users = mapped_users.sort_by do |u|
+      u[1]
+    end.reverse[0..4]
+
+    fast_map = users.map do |u|
+      g = u.games.min_by do |game|
+        # binding.pry
+        if game.status == 1
+          (game.updated_at - game.created_at)
+        else
+          9999
+        end
+      end
+      [g, (g.updated_at - g.created_at).round(2)]
+    end
+    @fastest = fast_map.sort_by do |u|
+      u[1]
+    end[0..4]
+    # binding.pry
   end
 
   private
